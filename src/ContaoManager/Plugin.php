1cf77f97f613da47ffdfc37d3ccd167b79e4c459
Sven Baumann <baumann.sv@gmail.com>
2019-04-26 16:12:12 +0200

    Init the bundle and the Contao manager

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of contaoblackforest/contao-encore-bundle.
+ *
+ * (c) 2014-2019 The Contao Blackforest team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    contaoblackforest/contao-encore-bundle
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2019 The Contao Blackforest team.
+ * @license    https://github.com/contaoblackforest/contao-encore-bundle/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace BlackForest\Contao\Encore\ContaoManager;
+
+use BlackForest\Contao\Encore\BlackForestContaoEncoreBundle;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Symfony\WebpackEncoreBundle\WebpackEncoreBundle;
+
+/**
+ * Contao Manager plugin.
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(BlackForestContaoEncoreBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class
+                    ]
+                ),
+            BundleConfig::create(WebpackEncoreBundle::class)
+                ->setLoadAfter(
+                    [
+                        ContaoCoreBundle::class,
+                        BlackForestContaoEncoreBundle::class
+                    ]
+                )
+        ];
+    }
+}
